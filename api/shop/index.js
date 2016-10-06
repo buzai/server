35e69c98d66aa8353be2746c30097d714a04ebcd
buzai <783243077@qq.com>
2016-10-06 15:55:26 +0800

    fix

--- api/shop/index.js
+++ api/shop/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express');
-var controller = require('./apply.controller');
+var controller = require('./shop.controller');
 // var auth = require('../../auth/auth.service');
 
 var router = express.Router();
