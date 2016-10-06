7171c53a7d01158a7642c44ff9e2bf420fa902ed
buzai <783243077@qq.com>
2016-10-06 11:59:46 +0800

    serve

---
+++ api/user/user.controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var User = require('./user.model');
+var passport = require('passport');
+var config = require('../../config/environment');
+var jwt = require('jsonwebtoken');
+
+var validationError = function(res, err) {
+  return res.status(422).json(err);
+};
+
+/**
+ * Creates a new user
+ */
+exports.create = function (req, res, next) {
+  console.log(req.body)
+  var newUser = new User(req.body);
+  newUser.provider = 'local';
+  newUser.role = 'user';
+  newUser.save(function(err, user) {
+    if (err) return validationError(res, err);
+    var token = jwt.sign({_id: user._id }, config.secrets.session, { expiresInMinutes: 60*5 });
+    res.json({ token: token });
+  });
+};
+
+exports.test = function (req, res) {
+  res.json('chenhao');
+}
---
+++ config/environment/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var path = require('path');
+
+function requiredProcessEnv(name) {
+  if(!process.env[name]) {
+    throw new Error('You must set the ' + name + ' environment variable');
+  }
+  return process.env[name];
+}
+
+// All configurations will extend these options
+// ============================================
+var all = {
+  env: process.env.NODE_ENV,
+
+  // Root path of server
+  root: path.normalize(__dirname + '/../../..'),
+
+  // Server port
+  port: process.env.PORT || 8080,
+
+  // Server IP
+  ip: process.env.IP || '127.0.0.1',
+
+  // Should we populate the DB with sample data?
+  seedDB: false,
+
+  // Secret for session, you will want to change this and make it an environment variable
+  secrets: {
+    session: 'angular-fullstack-secret'
+  },
+
+  // List of user roles
+  userRoles: ['guest', 'user', 'admin'],
+
+  // MongoDB connection options
+  mongo: {
+    options: {
+      db: {
+        safe: true
+      }
+    },
+    uri: 'mongodb://127.0.0.1/car-dev',
+    seedDB: true
+  }
+};
+
+// Export the config object based on the NODE_ENV
+// ==============================================
+module.exports = all
