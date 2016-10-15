e032cc927e4afb8ad86d5522ae200f6ccad2ce14
buzai <783243077@qq.com>
2016-10-15 23:03:49 +0800

    fix

--- routes.js
+++ routes.js
@@ -23,11 +23,11 @@ module.exports = function(app) {
    .get(errors[404]);
 
   // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendFile(path.normalize(__dirname+'/src') + '/index.html');
-      // res.render('index.html');
-    });
+  // app.route('/*')
+  //   .get(function(req, res) {
+  //     res.sendFile(path.normalize(__dirname+'/src') + '/index.html');
+  //     // res.render('index.html');
+  //   });
   app.route('/index').get(function(req, res, next) {
     console.log('index')
     res.sendFile(path.normalize(__dirname+'/index') + '/index.html');
